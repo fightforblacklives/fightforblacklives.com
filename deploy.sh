e32d1cea2a7ba8e6110b921b85f763475d03fd49
Samuel DeSota <me@samdesota.com>
2020-06-03 14:40:59 -0400

    update builds

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
-npm run export
-git checkout --orphan gh-pages
-git --work-tree __sapper__/export/fightforblacklives.com add --all
-git --work-tree __sapper__/export/fightforblacklives.com commit -m 'gh-pages'
-git push origin HEAD:gh-pages --force
+npm run export &&
+git branch -D gh-pages
+git checkout --orphan gh-pages &&
+git --work-tree __sapper__/export/fightforblacklives.com add --all &&
+git --work-tree __sapper__/export/fightforblacklives.com commit -m 'gh-pages' &&
+git push origin HEAD:gh-pages --force &&
 git checkout -f master
