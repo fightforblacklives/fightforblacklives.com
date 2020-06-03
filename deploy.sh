56cbd271c40b0c9d1c39784728453420c3070e01
Samuel DeSota <me@samdesota.com>
2020-06-03 14:54:57 -0400

    cleanup export

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,4 @@ git --work-tree __sapper__/export/fightforblacklives.com add --all &&
 git --work-tree __sapper__/export/fightforblacklives.com commit -m 'gh-pages' &&
 git push origin HEAD:gh-pages --force &&
 git checkout -f master
+rm -rf __sapper__/export
