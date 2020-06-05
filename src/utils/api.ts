751dd7b9c3240cd8d20d2f8fd858c9e353ccf497
Jon Bilous <jonbilous@jons-mbp-2.home>
2020-06-05 17:09:37 -0400

    api work

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -10,6 +10,27 @@ const getFetch = memoOnce(() => {
   return import("fetch-jsonp");
 });
 
+const fuzzyName = (name1, name2) => {
+  name1 = name1
+    .toLowerCase()
+    .trim()
+    .split(" ")
+    .map((x) => x.trim())
+    .filter((x) => x != null && x !== "");
+  name2 = new Set(
+    name2
+      .toLowerCase()
+      .trim()
+      .split(" ")
+      .map((x) => x.trim())
+      .filter((x) => x != null && x !== "")
+  );
+
+  const isMatch = name1.filter((n1) => name2.has(n1)).length > 1;
+  console.log(isMatch);
+  return isMatch;
+};
+
 const request = async (url: string) => {
   const { default: fetch } = await getFetch();
 
@@ -37,9 +58,15 @@ const googleCivicApi = async (address) => {
 
 export const getDataByAddress = async (zip) => {
   const googleCivicData = await googleCivicApi(zip + ", United States");
+  const addlData = await request(
+    `https://raw.githubusercontent.com/fightforblacklives/ffbl-data/master/zip-code-bundles/${zip}.jsonp`
+  );
+  const addlPeople = addlData.people.filter(
+    (x) => !googleCivicData.officials.some((y) => fuzzyName(y.name, x.name))
+  );
   const city = googleCivicData.normalizedInput.city;
   const state = googleCivicData.normalizedInput.state;
-  const people = googleCivicData.officials.flatMap((p, i) => {
+  const googlePeople = googleCivicData.officials.flatMap((p, i) => {
     const office = googleCivicData.offices.find((o) =>
       o.officialIndices.includes(i)
     );
@@ -52,7 +79,9 @@ export const getDataByAddress = async (zip) => {
     }
     return {
       name: p.name,
-      image: p.photoUrl,
+      image: p.photoUrl
+        ? p.photoUrl
+        : addlData.people.find((addl) => fuzzyName(addl.name, p.name))?.image,
       title: office.name,
       address:
         p.address && p.address.length > 0
@@ -66,14 +95,19 @@ ${p.address[0].city}, ${p.address[0].state}, ${p.address[0].zip}`
           : null,
       party: p.party,
       email: p.emails && p.emails.length > 0 ? p.emails[0] : null,
-      id: i,
+
       facebook:
         p.channels && p.channels.some((x) => x.type === "Facebook")
           ? p.channels.find((x) => x.type === "Facebook").id
           : null,
     };
   });
+  const people = [...googlePeople, ...addlPeople].map((x, i) =>
+    Object.assign({}, x, { id: i })
+  );
   console.log(googleCivicData);
+  console.log(addlData);
+
   return {
     city,
     state,
