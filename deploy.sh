de67da9b4c942cc87686f1e9aa064482e210cf2d
Samuel DeSota <me@samdesota.com>
2020-06-05 22:10:37 -0400

    change order of gh pages delete

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 npm run export &&
 echo "fightforblacklives.com" > __sapper__/export/CNAME &&
-git branch -D gh-pages &&
 git checkout --orphan gh-pages &&
 git --work-tree __sapper__/export/fightforblacklives.com add --all &&
 git --work-tree __sapper__/export/fightforblacklives.com commit -m 'gh-pages' &&
 git push origin HEAD:gh-pages --force &&
-git checkout -f master
-rm -rf __sapper__/export
+git checkout -f master &&
+git branch -D gh-pages 
+rm -rf __sapper__/export &&
