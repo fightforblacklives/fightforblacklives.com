fb50f7354c97342779c777f28aa4a2065af1ee95
Samuel DeSota <me@samdesota.com>
2020-06-05 22:11:58 -0400

    fix && location

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ git --work-tree __sapper__/export/fightforblacklives.com add --all &&
 git --work-tree __sapper__/export/fightforblacklives.com commit -m 'gh-pages' &&
 git push origin HEAD:gh-pages --force &&
 git checkout -f master &&
-git branch -D gh-pages 
-rm -rf __sapper__/export &&
+git branch -D gh-pages &&
+rm -rf __sapper__/export 
