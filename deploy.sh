7b4af0034c5822893ddaa2d7f096abc7d49a24d5
Samuel DeSota <me@samdesota.com>
2020-06-05 23:13:01 -0400

    gh-pages-publish

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 npm run export &&
 echo "fightforblacklives.com" > __sapper__/export/CNAME &&
-mv __sapper__/export docs &&
+rm -rf docs
+mv __sapper__/export/ docs &&
+git add . &&
 git commit -m 'gh-pages-publish' &&
 git push origin master
