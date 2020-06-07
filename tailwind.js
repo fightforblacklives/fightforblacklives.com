6ff0ecd57c73d2011e37c5216f89b97bf04a125f
Samuel DeSota <me@samdesota.com>
2020-06-07 12:06:43 -0400

    gh-pages-publish

--- tailwind.js
+++ tailwind.js
@@ -7,7 +7,7 @@ const makeColors = (colors) => {
 };
 
 module.exports = {
-  purge: ["./src/**/*.svelte", "./src/**/*.html"],
+  purge: ["./src/**/*.svelte", "./src/**/*.html", "./src/**/*.css"],
 
   theme: {
     fontFamily: {
