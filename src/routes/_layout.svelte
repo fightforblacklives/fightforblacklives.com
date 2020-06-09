47d6badcfa5dd7718871f902b861b28d7c4ddf38
Jon Bilous <jonbilous@jons-mbp-2.home>
2020-06-09 17:33:34 -0400

    gh-pages-publish

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -5,25 +5,17 @@
   import { fade } from "svelte/transition";
 
   export let segment;
-
-  let mounted = false;
-
-  onMount(() => {
-    mounted = true;
-  });
 </script>
 
-{#if mounted}
-  <div in:fade={{ duration: 200 }} class="min-h-screen flex flex-col">
-    <Nav {segment} />
+<div in:fade={{ duration: 200 }} class="min-h-screen flex flex-col">
+  <Nav {segment} />
 
-    <main class="flex self-stretch flex-1">
-      <slot />
-    </main>
+  <main class="flex self-stretch flex-1">
+    <slot />
+  </main>
 
-    <Footer class="mt-auto" />
-  </div>
-{/if}
+  <Footer class="mt-auto" />
+</div>
 
 <style>
   @import "../style/shared.css";
