51124df08c8d90645554768bb40976d423620cae
Samuel DeSota <me@samdesota.com>
2020-06-09 15:13:58 -0400

    gh-pages-publish

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,19 +1,29 @@
 <script>
   import Nav from "components/Nav";
   import Footer from "components/Footer";
+  import { onMount } from "svelte";
+  import { fade } from "svelte/transition";
 
   export let segment;
+
+  let mounted = false;
+
+  onMount(() => {
+    mounted = true;
+  });
 </script>
 
-<div class="min-h-screen flex flex-col">
-  <Nav {segment} />
+{#if mounted}
+  <div in:fade={{ duration: 200 }} class="min-h-screen flex flex-col">
+    <Nav {segment} />
 
-  <main class="flex self-stretch flex-1">
-    <slot />
-  </main>
+    <main class="flex self-stretch flex-1">
+      <slot />
+    </main>
 
-  <Footer class="mt-auto" />
-</div>
+    <Footer class="mt-auto" />
+  </div>
+{/if}
 
 <style>
   @import "../style/shared.css";
