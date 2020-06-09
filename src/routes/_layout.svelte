5ea693015176b0083af8b348a11189a508249d07
Samuel DeSota <me@samdesota.com>
2020-06-09 15:03:24 -0400

    gh-pages-publish

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -6,18 +6,6 @@
 </script>
 
 <div class="min-h-screen flex flex-col">
-  <div
-    class:hidden={process.env.NODE_ENV !== 'production'}
-    class="bg-c-bg-complement">
-    <div class="contained text-c-text-1-complement py-4">
-      This site is under construction. For more information, you can contact us
-      at
-      <a class="underline" href="mailto:contact@fightforblacklives.com">
-        contact@fightforblacklives.com
-      </a>
-    </div>
-  </div>
-
   <Nav {segment} />
 
   <main class="flex self-stretch flex-1">
